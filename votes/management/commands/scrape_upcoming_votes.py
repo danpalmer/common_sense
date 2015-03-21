5b548dc37807dc01d087ea54efca1c136993ae9e
Dan Palmer <dan.palmer@me.com>
2015-03-21 14:49:19 +0000

    Remove votes

--- votes/management/commands/scrape_upcoming_votes.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-from votes.models import Vote
-
-
-class Command(BaseCommand):
-
-    def handle(self, *args, **options):
-        raise NotImplementedError()
