eb777b56918bef649b604507dc9f74c8d46454cf
Dan Palmer <dan.palmer@me.com>
2015-03-21 17:29:31 +0000

    Some urls files

--- common_sense/urls.py
+++ common_sense/urls.py
@@ -1,6 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
+from accounts import urls as accounts_urls
+from consultations import urls as consultations_urls
+
+urlpatterns = patterns(
+    '',
+
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'', include(accounts_urls)),
+    url(r'', include(consultations_urls)),
 )
