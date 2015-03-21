f62d58fa811d7531c8b39536f4458d7d24f1bd73
Dan Palmer <dan.palmer@me.com>
2015-03-21 17:33:26 +0000

    Un-fuck-this-up

--- consultations/views.py
+++ consultations/views.py
@@ -1,16 +1,11 @@
-<<<<<<< HEAD
-from django.views.generic import ListView
+from django.views import generic as views
 
 from .models import Consultation
 
 
-class ConsultationsListView(ListView):
-    model = Consultation
-=======
-from django.shortcuts import render
-from django.views.generic import base as base_views
-
-
-class HomePageView(base_views.TemplateView):
+class HomePageView(views.TemplateView):
     template_name = 'consultations/home.html'
->>>>>>> origin/master
+
+
+class ConsultationsListView(views.ListView):
+    model = Consultation
