b10e5a4360a13b578dbbd62e90269dd89e22db2e
Dan Palmer <dan.palmer@me.com>
2015-03-21 22:32:50 +0000

    Merge migrations

---
+++ consultations/migrations/0013_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('consultations', '0012_consultation_last_update'),
+        ('consultations', '0012_auto_20150321_2218'),
+    ]
+
+    operations = [
+    ]
