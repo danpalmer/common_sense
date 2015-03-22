9eb0c62547193ec506e9fdc84fdef9a21a8ebb6c
Dan Palmer <dan.palmer@me.com>
2015-03-22 11:48:55 +0000

    Create topics from R output

--- consultations/management/commands/tag_documents.py
+++ consultations/management/commands/tag_documents.py
@@ -1,10 +1,28 @@
+import io
+import csv
+import subprocess
+
 from django.core.management.base import BaseCommand
 
-from consultations.models import Consultation
+from consultations.models import Consultation, Topic
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        for consultation in Consultation.objects.all():
-            pass
+        with io.String() as stdout:
+            subprocess.call("tag_documents.R", stdout=stdout)
+            csv_reader = csv.reader(stdout)
+            for consultation_id, tags in csv_reader:
+                consultation = Consultation.objects.get(pk=consultation_id)
+
+                consultation_topic_ids = []
+                for tag in [x.strip() for x in tags.split(',')]:
+                    topic = Topic.objects.get_or_create(name=tag)
+                    consultation.topics.get_or_create(topic=topic)
+
+                    consultation_topic_ids.append(topic.pk)
+
+                    Consultation.topics.exclude(
+                        topic__id__in=consultation_topic_ids,
+                    ).delete()
