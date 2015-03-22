0e1d616271e67916eee3c6324dda365f0a34b879
Florian Rathgeber <florian.rathgeber@gmail.com>
2015-03-22 12:44:13 +0000

    Smarter R init script

--- init.r
+++ init.r
@@ -1,1 +1,3 @@
-install.packages("tm", dependencies = TRUE)
+list.of.packages <- c("tm")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages, dependencies = TRUE)
