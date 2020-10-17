21d3342feecafac8a3b83810f79942f3d235f56d
Florian Rathgeber <florian.rathgeber@gmail.com>
2020-10-17 23:07:49 +0200

    Update consultations view

--- consultations/views.py
+++ consultations/views.py
@@ -11,19 +11,13 @@ class HomePageView(views.TemplateView):
     def get_context_data(self):
         context = super(HomePageView, self).get_context_data()
 
-        consultations = {
-            ConsultationStateEnum.from_value(s): c for s, c in
+        context.update({
+            state.slug: count for state, count in
             Consultation.objects.values_list(
                 'state'
             ).annotate(
                 models.Count('id'),
             )
-        }
-
-        context.update({
-            'open': consultations.get(ConsultationStateEnum.OPEN, 0),
-            'closed': consultations.get(ConsultationStateEnum.CLOSED, 0),
-            'finished': consultations.get(ConsultationStateEnum.OUTCOME, 0),
         })
 
         return context
